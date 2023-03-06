3065fefaf412ee47eae5fc3ec337e9a6080d705d
Gaston Castro <gaston10.c@hotmail.com>
2023-03-06 02:52:36 -0300

    ready to production

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,6 +72,15 @@ export default {
     mqtt_port:process.env.MQTT_PORT,
   },
 
+  server:{
+    port:3000,
+    host:'0.0.0.0'
+  },
+
+  serverMiddleware:{
+    'api':'~api'
+  },
+
   /*
   ** Build configuration
   */
